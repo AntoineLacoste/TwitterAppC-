e8b0fa950012d69cfd4c9affe5ef5ab09f2336a0
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2016-03-10 10:44:24 +0100

    ModelLocator implemented

---
+++ ViewModels/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterUniversalApp.ViewModels
+{
+    class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<ConnectionPageViewModel>();
+            SimpleIoc.Default.Register<TimeLinePageViewModel>();
+        }
+
+        public ConnectionPageViewModel Connection
+        {
+            get { return ServiceLocator.Current.GetInstance<ConnectionPageViewModel>(); }
+        }
+        public TimeLinePageViewModel TimeLine
+        {
+            get { return ServiceLocator.Current.GetInstance<TimeLinePageViewModel>(); }
+        }
+    }
+}
