5722c8778732f370e8d88997c8a22f9d2acf2d5e
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2016-03-11 17:27:53 +0100

    Delete tweet and post tweet imlemented

--- ViewModels/TimeLinePageViewModel.cs
+++ ViewModels/TimeLinePageViewModel.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.Xaml.Interactivity;
 using Tweetinvi.Core.Credentials;
 using Tweetinvi.Credentials;
 using Tweetinvi;
 using TwitterUniversalApp.Models;
 using CredentialsCreator = Tweetinvi.CredentialsCreator;
 using Tweet = Tweetinvi.Logic.Tweet;
+using User = Tweetinvi.User;
 
 namespace TwitterUniversalApp.ViewModels
 {
@@ -24,6 +32,9 @@ namespace TwitterUniversalApp.ViewModels
         {
             this.Selecteduser = (Tweetinvi.Logic.User) User.GetAuthenticatedUser();
             this.TimeLineTweets = getTimeLineObservableCollection(this.Selecteduser);
+            this._headerText = "Welcome @"+this.Selecteduser.Name;
+            this._nbCharacterTweet = 140;
+            this._stringPostTweet = "Characters left : "+this._nbCharacterTweet;
         }
 
         private string _pinInput;
@@ -32,7 +43,14 @@ namespace TwitterUniversalApp.ViewModels
             get { return _pinInput; }
             set { Set(ref _pinInput, value); }
         }
+        
 
+         private string _headerText;
+        public string HeaderText
+        {
+            get { return _headerText; }
+            set { Set(ref _headerText, value); }
+        }
         private Tweetinvi.Logic.User _selecteduser;
         public Tweetinvi.Logic.User Selecteduser
         {
@@ -131,20 +149,93 @@ namespace TwitterUniversalApp.ViewModels
             Tweetinvi.Tweet.FavoriteTweet(Int64.Parse(idTweet));
         }
 
-        private RelayCommand _getPinCmd;
-        public RelayCommand GetCmd
+        private RelayCommand<string> _deleteTweetCmd;
+        public RelayCommand<string> DeleteTweetCmd
         {
             get
             {
-                if (_getPinCmd == null)
-                    _getPinCmd = new RelayCommand(GetConnection);
-                return _getPinCmd;
+                if (_deleteTweetCmd == null)
+                    _deleteTweetCmd = new RelayCommand<string>(Delete);
+                return _deleteTweetCmd;
             }
         }
 
-        public void GetConnection()
+        public void Delete(string idTweet)
         {
-            this.NavigationService.Navigate(typeof(Views.PinPage));
+            var id = Int64.Parse(idTweet);
+            var tweet = this._timeLinetweets.First(t => t.Id == id);
+            if (tweet.CreatedBy == this._selecteduser)
+            {
+                this._timeLinetweets.Remove((Tweet)tweet);
+                Tweetinvi.Tweet.DestroyTweet(id);
+            }
+        }
+
+        private RelayCommand _postTweetCmd;
+        public RelayCommand PostTweetCmd
+        {
+            get
+            {
+                if (_postTweetCmd == null)
+                    _postTweetCmd = new RelayCommand(PostTweet);
+                return _postTweetCmd;
+            }
+        }
+
+        private int _nbCharacterTweet;
+
+        private string _stringPostTweet;
+
+        public string StringPostTweet
+        {
+            get { return _stringPostTweet; }
+            set { Set(ref _stringPostTweet, value); }
+        }
+
+        public async void PostTweet()
+        {
+            var msg = new ContentDialog();
+
+            var stackPanel = new StackPanel();
+
+            var textBox = new TextBox();
+            textBox.KeyUp += new KeyEventHandler(checkTweetCharacter);
+            textBox.MaxLength = 140;
+            stackPanel.Children.Add(textBox);
+
+            var textBlock = new TextBlock
+            {
+                Text = this.StringPostTweet
+            };
+            var binding = new Binding();
+            binding.Source = this;
+            binding.Path = new PropertyPath("StringPostTweet");
+            textBlock.SetBinding(TextBlock.TextProperty, binding);
+            stackPanel.Children.Add(textBlock);
+
+            msg.Content = stackPanel;
+
+            msg.Title = "Post a tweet";
+            msg.PrimaryButtonText = "Post";
+            msg.SecondaryButtonText = "Cancel";
+            var contentDialog = await msg.ShowAsync();
+            switch (contentDialog)
+            {
+                case ContentDialogResult.Primary:
+                    var a = 5;
+                    break;
+                case ContentDialogResult.Secondary:
+                case ContentDialogResult.None:
+                    break;
+            }
+        }
+
+        private void checkTweetCharacter(object sender, KeyRoutedEventArgs e)
+        {
+            var textBox = (TextBox) sender;
+
+            this._nbCharacterTweet = 140 - textBox.Text.Length;
+            this.StringPostTweet = "Characters left : " + this._nbCharacterTweet;
         }
     }
 }
