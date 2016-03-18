7a72c36dcbc184e2c8eb4eb3de3882a241ed2b6e
AntoineLacoste <antoine.lacoste@iut-valence.fr>
2016-03-18 13:56:14 +0100

    Using updated

--- Models/TwitterConnectionInfo.cs
+++ Models/TwitterConnectionInfo.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tweetinvi.Core.Credentials;
+﻿using Tweetinvi.Core.Credentials;
 
 namespace TwitterUniversalApp.Models
 {
